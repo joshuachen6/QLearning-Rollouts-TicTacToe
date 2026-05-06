4d185b68f2de9efbaba3b9bad512cd75a60ecd30
joshuachen6 <jhc3628@gmail.com>
2026-05-06 00:52:58 -0500

    Fixed xmake

--- xmake.lua
+++ xmake.lua
@@ -6,7 +6,6 @@ option("cuda")
     set_showmenu(true)
     set_description("Enable CUDA support")
     on_check(function (option)
-        import("core.base.os")
         if os.execv("nvidia-smi") == 0 then
             option:set_value(true)
         end
@@ -17,23 +16,47 @@ add_requires("sqlite3", "toml11", "openmp")
 
 target("TicTacToe")
     set_kind("binary")
-    add_files("src/*.cpp")
-    add_includedirs("include")
     
-    -- Use the libtorch directory (manually managed or symlinked)
-    if os.isdir("libtorch") then
-        add_includedirs("libtorch/include")
-        add_includedirs("libtorch/include/torch/csrc/api/include")
-        add_linkdirs("libtorch/lib")
-        add_links("torch", "torch_cpu", "c10")
-        if get_config("cuda") then
-            add_links("torch_cuda")
+    on_load(function (target)
+        -- Check if libtorch is present in the project directory
+        local libtorch_dir = path.join(os.projectdir(), "libtorch")
+        if not os.isdir(libtorch_dir) then
+            print("LibTorch not found. Downloading...")
+            local url
+            if is_plat("linux") then
+                if get_config("cuda") then
+                    url = "https://download.pytorch.org/libtorch/cu124/libtorch-shared-with-deps-2.5.1%2Bcu124.zip"
+                else
+                    url = "https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.5.1%2Bcpu.zip"
+                end
+            elseif is_plat("macosx") then
+                url = "https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-2.5.1.zip"
+            end
+            
+            if url then
+                os.runv("curl", {"-L", url, "-o", "libtorch.zip"})
+                os.runv("unzip", {"-q", "libtorch.zip"})
+                os.rm("libtorch.zip")
+            else
+                raise("Platform not supported for auto-download")
+            end
         end
         
-        add_runenvs("LD_LIBRARY_PATH", os.projectdir() .. "/libtorch/lib")
-        add_runenvs("DYLD_LIBRARY_PATH", os.projectdir() .. "/libtorch/lib")
-    end
+        target:add("includedirs", "libtorch/include")
+        target:add("includedirs", "libtorch/include/torch/csrc/api/include")
+        target:add("linkdirs", "libtorch/lib")
+        target:add("links", "torch", "torch_cpu", "c10")
+        
+        if get_config("cuda") then
+            target:add("links", "torch_cuda")
+        end
+        
+        target:add("runenvs", "LD_LIBRARY_PATH", path.join(os.projectdir(), "libtorch/lib"))
+        target:add("runenvs", "DYLD_LIBRARY_PATH", path.join(os.projectdir(), "libtorch/lib"))
+    end)
 
+    add_files("src/*.cpp")
+    add_includedirs("include")
     add_packages("sqlite3", "toml11", "openmp")
     set_languages("c++17")
     
